ddf82985a2a4d996bba2cc93bed0d1e085f966fe
therealPatrick <malamashangala13@gmail.com>
2023-11-04 21:15:36 +0200

    comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,23 +2,22 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-  
+
   def show
     @article = Article.find(params[:id])
   end
-  
+
   def new
     @article = Article.new
   end
-  
+
   def create
-    # @article = Article.new(title: "...", body: "...")
     @article = Article.new(article_params)
-    
+
     if @article.save
       redirect_to @article
     else
-      render new, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -36,16 +35,7 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
-
-    redirect_to root_path, status: :see_other
-  end
-
-  
   private
-
     def article_params
       params.require(:article).permit(:title, :body)
     end
